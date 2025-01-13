a6c5a580f00f28f921cbd5cfefdc207614deab2a
lacroixpaul <paul.lacroix892@outlook.fr>
2025-01-13 09:51:21 +0100

    Pycoding style corrected

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
+
 import sys
+
 
 def factorial(n):
     result = 1
@@ -7,6 +10,7 @@ def factorial(n):
         result *= n
         n -= 1  # Décrémenter n à chaque itération
     return result
+
 
 f = factorial(int(sys.argv[1]))
 print(f)
